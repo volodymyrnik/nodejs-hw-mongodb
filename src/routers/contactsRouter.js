c331e426994bb96a378f8f297e9ad27f9255bf13
Volodymyr Nikiforenko <vl.nikiforenko@gmail.com>
2025-06-12 13:25:10 +0300

    Fix: add named export contactsRouter

--- src/routers/contactsRouter.js
+++ src/routers/contactsRouter.js
@@ -1,21 +1,18 @@
 import express from 'express';
 import {
-  getContactsController,
-  getContactByIdController,
-  deleteContactController,
-  createContactController,
-  updateContactController,
-} from '../controllers/contacts.js';
-import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+  getAllContacts,
+  getContactById,
+  createContact,
+  updateContact,
+  deleteContact,
+} from '../controllers/contactsController.js';
 
 const router = express.Router();
-const jsonParser = express.json();
 
-router.get('/', ctrlWrapper(getContactsController));
+router.get('/', getAllContacts);
+router.get('/:id', getContactById);
+router.post('/', createContact);
+router.put('/:id', updateContact);
+router.delete('/:id', deleteContact);
 
-router.get('/:id', ctrlWrapper(getContactByIdController));
-router.post('/', jsonParser, ctrlWrapper(createContactController));
-router.patch('/:id', jsonParser, ctrlWrapper(updateContactController));
-router.delete('/:id', ctrlWrapper(deleteContactController));
-
-export default router;
+export const contactsRouter = router; // ✅ Іменований експорт
