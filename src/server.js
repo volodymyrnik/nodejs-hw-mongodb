2aff7c633f78f772674540e166946a3ef909385f
Volodymyr Nikiforenko <vl.nikiforenko@gmail.com>
2025-06-22 00:59:25 +0300

    hw7-swagger

--- src/server.js
+++ src/server.js
@@ -10,16 +10,20 @@ import contactRouter from './routers/contacts.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { authenticate } from './middlewares/authenticate.js';
+import * as fs from 'node:fs';
 import path from 'node:path';
+import swaggerUI from 'swagger-ui-express';
 
 
 const PORT = Number(process.env.PORT || 3000);
+const SWAGGER_DOCUMENT = JSON.parse(
+  fs.readFileSync(path.join('docs', 'swagger.json'), 'utf-8'),
+);
 
 export const setupServer = () => {
   const app = express();
 
   app.use(cors());
-  app.use('/photos', express.static(path.resolve('uploads', 'photos')));
   app.use(cookieParser());
 
   app.use(express.json());
@@ -31,6 +35,12 @@ export const setupServer = () => {
       },
     })
   );
+
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(SWAGGER_DOCUMENT));
+
+  app.use('/photos', express.static(path.resolve('uploads', 'photos')));
+
+
 
   // ✅ Роути
   app.use('/auth', authRouter);
